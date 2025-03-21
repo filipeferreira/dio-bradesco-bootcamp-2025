b47d50fcaa55f4fca17d43ee8d04a6fb0ee14d9e
Filipe Ferreira <filipef@ciasc.sc.gov.br>
2025-03-21 07:53:15 -0300

    Initial commit

---
+++ src/main/java/me/dio/bradesco_bootcamp_2025/BradescoBootcamp2025Application.java
@@ -0,0 +1,13 @@
+package me.dio.bradesco_bootcamp_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BradescoBootcamp2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BradescoBootcamp2025Application.class, args);
+	}
+
+}
