b6f8742293b3e3f7ee843b5127f8ea4064c8aa44
Filipe Ferreira <filipef@ciasc.sc.gov.br>
2025-03-21 10:08:24 -0300

    Creating the entities

---
+++ src/main/java/me/dio/bradesco_bootcamp_2025/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package me.dio.bradesco_bootcamp_2025.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/me/dio/bradesco_bootcamp_2025/domain/model/News.java
@@ -0,0 +1,8 @@
+package me.dio.bradesco_bootcamp_2025.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
