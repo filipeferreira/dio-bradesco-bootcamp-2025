a054490fdfb144474dd1a6d85ea5b07b29d51ddf
Filipe Ferreira <filipef@ciasc.sc.gov.br>
2025-03-22 09:24:00 -0300

    railway fix: CORS,Network Failure,URL scheme must be "http" or https" for CORS request.

--- src/main/java/me/dio/bradesco_bootcamp_2025/BradescoBootcamp2025Application.java
+++ src/main/java/me/dio/bradesco_bootcamp_2025/BradescoBootcamp2025Application.java
@@ -1,8 +1,13 @@
 package me.dio.bradesco_bootcamp_2025;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {
+		@Server(url = "/", description = "Default server url")
+})
 @SpringBootApplication
 public class BradescoBootcamp2025Application {
 
